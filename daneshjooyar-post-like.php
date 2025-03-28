4aff55fd28268177ecb291c0b3442f7048f75385
Amir-Nourani <nouraniseo@gmail.com>
2025-03-27 17:14:04 -1200

    create folders

---
+++ daneshjooyar-post-like.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Daneshjooyar Post Like
+ * Plugin URI: https://daneshjooyar.com
+ * Description: This plugins for like posts
+ * Author: Hamed Moodi
+ * Author URI: https://daneshjooyar.com/teacher/h_m_mood
+ * Text Domain: daneshjooyar-post-like
+ * Domain Path: /languages
+ */
+
+ defined( 'ABSPATH' ) || exit;
